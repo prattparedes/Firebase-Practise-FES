87b7108bd009c4bcd6196c6d14a2535c1838751d
Pratt <pratt.paredes@gmail.com>
2022-10-15 00:56:37 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { auth } from "./firebase/init";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+function register() {
+  console.log("register");
+  createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+    .then((user) => {
+      console.log(user);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={register}>Register</button>
     </div>
   );
 }
---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products t;hat you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpKRH0tDNWFv9Hcbjga7PhKsUyLdquSC0",
+  authDomain: "fir-practise-fes.firebaseapp.com",
+  projectId: "fir-practise-fes",
+  storageBucket: "fir-practise-fes.appspot.com",
+  messagingSenderId: "383713631305",
+  appId: "1:383713631305:web:94f68575e0e54f632bb398"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
