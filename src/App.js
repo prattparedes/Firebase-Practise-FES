85b5fbd664a49250dc769b55f1d5d4201b600ceb
Pratt <pratt.paredes@gmail.com>
2022-10-15 16:51:31 -0500

    logout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
+import React, { useState } from 'react'
 import "./App.css";
 import { auth } from "./firebase/init";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
-function register() {
-  console.log("register");
-  createUserWithEmailAndPassword(auth, "email@email.com", "test123")
-    .then((user) => {
-      console.log(user);
+
+function App() {
+  const [user, setUser] = React.useState({})  
+ 
+  function register() {
+    console.log("register");
+    createUserWithEmailAndPassword(auth, "moises@email.com", "negro123")
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  
+  function login() {
+    signInWithEmailAndPassword(auth, "moises@email.com", "negro123")
+    .then(({ user }) => {
+      console.log(user)
+      setUser(user)
     })
     .catch((error) => {
       console.log(error);
     });
-}
+  }
 
-function App() {
+  function logout() {
+    signOut(auth);
+    setUser({})
+  }
+
   return (
     <div className="App">
       <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {user.email}
     </div>
   );
 }
